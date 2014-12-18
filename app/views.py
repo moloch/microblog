cb1dfc6f90485f177dc82183b550f72298d603d6
Dario Coco <dario.coco@gmail.com>
2014-12-18 13:29:08 +0000

    Part VI 2/3

--- app/views.py
+++ app/views.py
@@ -51,6 +51,19 @@ def after_login(resp):
     login_user(user, remember = remember_me)
     return redirect (request.args.get('next') or url_for('index'))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
